4b6def7058e97d45fd80060bb1900c2b62384a94
kumak5 <my0yashik@gmail.com>
2023-09-28 16:40:17 +0400

    add style on click

--- src/components/todo-list-item/Todo-list-item.jsx
+++ src/components/todo-list-item/Todo-list-item.jsx
@@ -1,14 +1,33 @@
-import React from "react";
+import React,{useState} from "react";
 import './todo-list-item.css'
 
-const TodoListItem =({label, important = false})=>{
-    const style ={
-        color: important? 'tomato' : 'black'
+const TodoListItem =({label, important}) => {
+    
+    const [done, setDone] = useState(false)
+
+    const style = {
+        color: important? 'tomato' : 'black',
+        fontWeight: important? 'bold' : 'normal'
     }
 
-    return <span className="todo-list-item" 
-                style={style}>
+    
+
+    const onLabelClick = () => {
+        console.log(`${label} was clicked`)
+        setDone(!done)
+    }
+
+    let classNames = 'todo-list-item'
+    if(done){
+        classNames = classNames + ' done'
+    }
+
+    return <span className={classNames} >
+                <span className="todo-list-item-label"
+            style={style}
+            onClick={onLabelClick}>
         {label}
+        </span>
     </span>
 }
 
